26f44e84e4a05983fab1f22a3d64a4607bf0932f
John Mark Causing <johnmarkcausing@gmail.com>
2021-06-11 10:06:58 +0800

    added PHP Philippine Pesos display

--- ethbal.5m.sh
+++ ethbal.5m.sh
@@ -16,4 +16,15 @@ eth_bal=$($JQ -n $balance_none_eth/$convert) # Conversion to ETH
 
 a_rounded=`printf "%.6f" $eth_bal` # Lessen decimal to display the same in 2miners website
 
-echo ETH Bal: $a_rounded # Display real ETH Balance
+# Get updated currency ETH rate to PHP (Philippine Peso)
+
+ethphprate=$(curl -s "https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=PHP")
+
+ethphp=$($JQ -r ".PHP" <<< $ethphprate) # Get current 1ETH to PHP value
+
+RES=$(echo "scale=2; $ethphp*$eth_bal" | bc) # Convert ETH balance to PHP value
+
+eth_php_bal=`printf "%.0f" $RES` # Lessen decimal to display the same in 2miners website
+
+echo ETH Bal: $a_rounded [ PHP: $eth_php_bal ] # Display real ETH Balance
+
